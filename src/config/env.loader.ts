12a3083f3b4929a2403486c75be0bd57717bce7a
Matias Gallardo <matiasgallardo196@gmail.com>
2025-11-25 13:47:30 +1100

    Enhance API with Swagger documentation and health check endpoint
    - Added Swagger support to the application for API documentation.
    - Configured Swagger in main.ts to serve documentation at '/docs' in development mode.
    - Implemented a health check endpoint in HealthController to return application status.
    - Updated environment loader to include CORS configuration and frontend URL.
    - Added new dependencies: @nestjs/swagger and swagger-ui-express in package.json and package-lock.json.
    - Removed unused health.controller.spec.ts test file.

--- src/config/env.loader.ts
+++ src/config/env.loader.ts
@@ -2,12 +2,24 @@ import { config, DotenvConfigOptions } from 'dotenv';
 
 const dotenvConfig = config as (options: DotenvConfigOptions) => void;
 
+// Environment variables
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 const envFile = `.env.${NODE_ENV}`;
 dotenvConfig({ path: envFile });
 
+// Environment flags
+export const IS_PRODUCTION = NODE_ENV === 'production';
+export const IS_TEST = NODE_ENV === 'test';
+export const IS_DEVELOPMENT = NODE_ENV === 'development';
+
+// Server variables
 export const PORT = process.env.PORT || 3000;
 
+// Database variables
 export const DATABASE_URL = process.env.DATABASE_URL;
 
+// Frontend variables
 export const FRONTEND_URL = process.env.FRONTEND_URL;
+
+// CORS variables
+export const CORS_ORIGIN = process.env.CORS_ORIGIN;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config/env.loader';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { IS_DEVELOPMENT } from './config/env.loader';
+import { CORS_ORIGIN } from './config/env.loader';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Boiler Plate Nest JS')
+    .setDescription('Boiler Plate Nest JS API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  if (IS_DEVELOPMENT) {
+    SwaggerModule.setup('docs', app, document);
+  }
+
+  // CORS configuration
+  app.enableCors({
+    origin: CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(PORT);
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Docs are running on port ${PORT}/docs`);
 }
+
 bootstrap();
