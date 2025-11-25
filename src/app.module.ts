5229f6fb835c17bae9eb4ebf60545c04701297da
Matias Gallardo <matiasgallardo196@gmail.com>
2025-11-25 18:00:59 +1100

    Add class-transformer and class-validator dependencies, and implement global exception handling
    - Added class-transformer and class-validator to package.json and package-lock.json for data transformation and validation.
    - Integrated AllExceptionsFilter in AppModule to handle exceptions globally across the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { AppLoggerModule } from './common/logger/logger.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [AppLoggerModule, HealthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
