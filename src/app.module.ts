d62b05b522253f1f1d0fe4e124d2dd2f7800bfae
Matias Gallardo <matiasgallardo196@gmail.com>
2025-11-25 23:41:37 +1100

    Implement global validation pipe and update AppModule configuration
    - Added AppValidationPipe for enhanced request validation and transformation.
    - Integrated AppValidationPipe into AppModule to enforce validation rules globally.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { AppLoggerModule } from './common/logger/logger.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { AppValidationPipe } from './common/pipes/app-validation.pipe';
 
 @Module({
   imports: [AppLoggerModule, HealthModule],
@@ -11,6 +12,10 @@ import { APP_FILTER } from '@nestjs/core';
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: AppValidationPipe,
     },
   ],
 })
